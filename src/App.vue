6406f56eecd10b73ed38ecdefcb76c80fbcff1e3
王哲钦 <wangzqac@navimentum.com>
2022-10-09 16:20:29 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <a-layout>
+    <a-layout-sider>Sider</a-layout-sider>
+    <a-layout>
+      <a-layout-header>Header</a-layout-header>
+      <a-layout-content>Content</a-layout-content>
+      <a-layout-footer>Footer</a-layout-footer>
+    </a-layout>
+  </a-layout>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+#components-layout-demo-basic .ant-layout-content {
+  min-height: 120px;
+  color: #fff;
+  line-height: 120px;
+  background: rgba(16, 142, 233, 1);
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+#components-layout-demo-basic > .code-box-demo > .ant-layout + .ant-layout {
+  margin-top: 48px;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+const Vue = createApp(App).use(store).use(router);
+
+import {
+    Button,
+    Layout,
+    Menu,
+} from 'ant-design-vue'
+
+Vue.component(Button.name,Button)
+Vue.component(Layout.name,Layout)
+Vue.component(Menu.name,Menu)
+
+Vue.mount('#app')
+
